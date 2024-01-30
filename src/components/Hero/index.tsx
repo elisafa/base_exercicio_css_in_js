ea9dffdc0eec976287be2d6430556be1b1831581
Elisafa <elisafajunior@hotmail.com>
2024-01-30 20:43:12 -0300

    Estilizado o hero com styled-components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { FormularioHero, HeroTitulo } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <FormularioHero>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <HeroTitulo>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </HeroTitulo>
     </div>
-  </form>
+  </FormularioHero>
 )
 
 export default Hero
